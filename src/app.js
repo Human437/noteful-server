962267e87192111c0e558fe85e25e0a7ceca5325
Ryan <leeyuenhigh@gmail.com>
2021-01-14 18:45:35 -0500

    Imported necessary components

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const foldersRouter = require('./folders/folders-router')
+const notesRouter = require('./notes/notes-router')
 
 const app = express()
 
@@ -11,9 +14,12 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'dev';
 
+app.use('/api/folders', foldersRouter)
+app.use('/api/notes', notesRouter)
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(validateBearerToken)
 
 app.get('/',(req,res)=>{
   res.send('Hello, world!')
