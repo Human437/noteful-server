5876634e9be8f68fabb4382ab68cb4b8d7405741
Ryan <leeyuenhigh@gmail.com>
2021-01-15 02:21:09 -0500

    Fixed folderId issue

--- seeds/seed.noteful.notes.sql
+++ seeds/seed.noteful.notes.sql
@@ -2,7 +2,7 @@ INSERT INTO noteful_notes(name,modified,folderId,content)
 VALUES
     ('Dogs','2029-01-22T16:28:32.615Z',1,'Content about dogs'),
     ('Birds','2100-05-22T16:28:32.615Z',3,'Content about birds'),
-    ('Cats','1919-12-22T16:28:32.615Z',4,'Content about cats'),
+    ('Cats','1919-12-22T16:28:32.615Z',2,'Content about cats'),
     ('Rats','1919-12-22T16:28:32.615Z',1,'Content about rats'),
     ('Cows','1999-12-22T16:28:32.615Z',3,'Content about cows')
 ;
